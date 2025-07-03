9d5779570d6c7fdbd647598de87756bb8c52afc1
gabriel Silva <gabriel_17_silva@outlook.com>
2025-07-02 22:44:23 -0300

    Barra de navegação

---
+++ Grandion Fast Food/Controllers/ContatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Grandion_Fast_Food.Controllers
+{
+    public class ContatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
